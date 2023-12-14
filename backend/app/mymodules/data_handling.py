14f9b40a7739f9e191394912fdca35610ff7bf63
fedeMDR <888863@stud.unive.it>
2023-12-14 15:23:22 +0100

    Commented data_handling module

--- backend/app/mymodules/data_handling.py
+++ backend/app/mymodules/data_handling.py
@@ -1,25 +1,42 @@
 import pandas as pd
 
-# Creating dataframes with pandas
-attractions = pd.read_csv('Datasets/Location.csv')
-crime = pd.read_csv('Datasets/CrimeCount.csv')
-stations = pd.read_csv('Datasets/Stations.csv')
-trees = pd.read_csv('Datasets/Trees.csv')
-zip = pd.read_csv('Datasets/zip_neighbourhood.csv')
-bnb = pd.read_csv('Datasets/AirBnb.csv')
+AIRBNB = pd.read_csv('Datasets/AirBnb.csv')
+ATTRACTION = pd.read_csv('Datasets/Location.csv')
+CRIME = pd.read_csv('Datasets/CrimeCount.csv')
+TREES = pd.read_csv('Datasets/Trees.csv')
+NEIGHBOURHOODS = ['Manhattan', 'Bronx', 'Queens', 'Staten Island', 'Brooklyn']
 
-def convert_price(price_str):
+def convert_price(price_str : str):
+    '''
+    convert the price in Airbnb dataframe from a string to an integers
+
+    Arg:
+        price (str) : the price as a string
+    
+    Returns:
+        int : the price converted to int
+    '''
     prezzo_float = float(price_str.replace('$', '').replace(',', ''))
 
     return int(prezzo_float)
 
-bnb['price'] = bnb['price'].apply(convert_price)
+#converting price of our datasets from string to float
+AIRBNB['price'] = AIRBNB['price'].apply(convert_price)
 
 
 # Finds zipcodes that respects the criteria for the number of attractions
-def corrZipAtt(min, max):
-    # if(min>=0 and max<=20):
-    counts_by_zipcode = attractions.groupby('Zipcode')['Tourist_Spot'].count().reset_index()
+def corrZipAtt(min : int, max : int):
+    '''
+    Count the number of attractions for each zipcodes and filters the zipcodes that have at leat {min} attractions and at most {max} attractions.
+    
+    Args:
+        min (int) : the minimum amount of attraction that the user want to see.
+        max (int) : the maximum amount of attractin that the user want to see.
+    
+    Returns:
+        list : list of the zipcodes that contains at least the number of attraction that the user want to see.
+    '''
+    counts_by_zipcode = ATTRACTION.groupby('Zipcode')['Tourist_Spot'].count().reset_index()
     filtered_locations = counts_by_zipcode[(counts_by_zipcode['Tourist_Spot'] >= min) & (counts_by_zipcode['Tourist_Spot'] <= max)]
     zipcodes_attr = filtered_locations['Zipcode'].tolist()
     
@@ -27,60 +44,109 @@ def corrZipAtt(min, max):
 
 
 # Finds zipcodes that respects the criteria for green areas
-def corrZipTrees(trees_bool):
+def corrZipTrees(trees_bool : str):
+    '''
+    Compute the mean number of trees among all NYC neighbourhoods and return a list of zipcodes that match the user request.
+
+    Arg:
+        trees_bool (bool) : value that reflect if the user wants to stay in a green area or not.
+    
+    Returns:
+        list : 
+                if True : all the zipcodes that have a number of trees that is above the mean of NYC.
+                if False : returns every zipcodes.
+    '''
     if trees_bool == 'True' or trees_bool == 'False':
         if(trees_bool == 'True'):
-            trees_mean = int(trees['count'].mean())
-            zipcodes_trees = trees[trees['count'] >= trees_mean]['zipcode'].tolist()
+            trees_mean = int(TREES['count'].mean())
+            zipcodes_trees = TREES[TREES['count'] >= trees_mean]['zipcode'].tolist()
             return zipcodes_trees
         elif(trees_bool == 'False'):
-            zipcodes_trees = trees['zipcode'].tolist()
+            zipcodes_trees = TREES['zipcode'].tolist()
             return zipcodes_trees
     else:
         raise ValueError()
 
 
 # Finds zipcodes that respects the criteria for crime rates
-def corrZipCrime(crime_rate):
-    # if isinstance(crime_rate, int):
-        # if(crime_rate>=1 or crime_rate<=4):
-            if(crime_rate == 4):
-                crime_threshold = int(((crime['count'].max()-crime['count'].min())/4)+crime['count'].min())
-                zipcodes_crime = crime[crime['count'] <= crime_threshold]['zipcode'].tolist()
-            elif(crime_rate == 3):
-                crime_threshold = int(((crime['count'].max()-crime['count'].min())/2)+crime['count'].min())
-                zipcodes_crime = crime[crime['count'] <= crime_threshold]['zipcode'].tolist()
-            elif(crime_rate == 2):
-                crime_threshold = int(((((crime['count'].max()-crime['count'].min())/4))*3)+crime['count'].min())
-                zipcodes_crime = crime[crime['count'] <= crime_threshold]['zipcode'].tolist()
-            elif(crime_rate == 1):
-                zipcodes_crime = crime['zipcode'].tolist()
-            return zipcodes_crime
-        # else:
-        #     raise ValueError("Input crime_rate must be an integer.")
+def corrZipCrime(crime_rate : int):
+    '''
+    Computes the crime rate threshold for each level of crime (from 1 to 4)
+    
+    Args:
+        crime_rate (int) : a value from 1 to 4 given by the user depending on how much he cares about crimes.
+    
+    Returns: 
+        list : a list of the zipcodes that comply with the crime level selected
+    '''
+    if(crime_rate == 4):
+        crime_threshold = int(((CRIME['count'].max()-CRIME['count'].min())/4)+CRIME['count'].min())
+        zipcodes_crime = CRIME[CRIME['count'] <= crime_threshold]['zipcode'].tolist()
+    elif(crime_rate == 3):
+        crime_threshold = int(((CRIME['count'].max()-CRIME['count'].min())/2)+CRIME['count'].min())
+        zipcodes_crime = CRIME[CRIME['count'] <= crime_threshold]['zipcode'].tolist()
+    elif(crime_rate == 2):
+        crime_threshold = int(((((CRIME['count'].max()-CRIME['count'].min())/4))*3)+CRIME['count'].min())
+        zipcodes_crime = CRIME[CRIME['count'] <= crime_threshold]['zipcode'].tolist()
+    elif(crime_rate == 1):
+        zipcodes_crime = CRIME['zipcode'].tolist()
+    return zipcodes_crime
+
     
 
 
 # Finds common zipcodes among three lists
 def commonZip(zip_1, zip_2, zip_3):
+    '''
+    finds all the common zipcodes among the three given lists
+
+    Args:
+        zip_1 (list) : a list of zipcodes
+        zip_2 (list) : a list of zipcodes
+        zip_3 (list) : a list of zipcodes
+
+    Returns : 
+        list : a list of the common zipcodes among the three lists
+    '''
     res = set(zip_1) & set(zip_2) & set(zip_3)
     return list(res)
     
 
 # Find the cheapest zipcode for each zipcode in the list
 def BnbPerZip(zip_list, bnb_df):
+    '''
+    finds all the airbnb that are located inside a given zipcode
+
+    Args:
+        zip_list (list) : a list of zipcodes.
+        bnb_df (pd.DataFrame) : A Dataframe containing at least airbnbs and their zipcodes.
+
+    Returns:
+        pandas DataFrame : a subset of our original dataframe containing only the selected airbnbs
+    '''
     airbnb_df = bnb_df[bnb_df['zipcode'].isin(zip_list)]
     
     return airbnb_df
 
 
-def get_bnb_by_neighborhood(target_neighborhood):
-    neighborhood_list = ['Manhattan', 'Bronx', 'Queens', 'Staten Island', 'Brooklyn']
-    if target_neighborhood not in neighborhood_list:
-        target_neighborhood == None
-    if not target_neighborhood:
-        raise ValueError("Input must be a string.")
-    if target_neighborhood in neighborhood_list:
-        neighborhood_df = bnb[bnb['neighbourhood_group_cleansed'] == target_neighborhood]
-        return neighborhood_df.head(50)
+def get_bnb_by_neighborhood(target_neighbourhood):
+    '''
+    Get all the airbnbs that are located inside the given neighbourhood.
+
+    Args:
+        Target_neighbourhoods (str) : a neighbourhood name
     
+    Raises:
+        ValueError : if the target neighbourhood is empty or is not a neighbourhood of NYC.
+    
+    Returns:
+        padas DataFrame : the first 50 airbnbs located inside the given neighbourhood.
+
+    '''
+    if target_neighbourhood not in NEIGHBOURHOODS:
+        target_neighbourhood == None
+    if not target_neighbourhood:
+        raise ValueError("Input must be a string and must be a NYC neighbourhood.")
+    if target_neighbourhood in NEIGHBOURHOODS:
+        neighborhood_df = AIRBNB[AIRBNB['neighbourhood_group_cleansed'] == target_neighbourhood]
+        return neighborhood_df.head(50)
