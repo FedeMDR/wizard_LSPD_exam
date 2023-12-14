12e93ceafda508ba0bf7cf377d940f75803be02f
martinaleccia <martina.aleccia@student.h-farm.com>
2023-12-14 15:32:58 +0100

    commented backend/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,24 +10,37 @@ app = FastAPI()
 
 
 # Creating dataframes with pandas
-attractions = pd.read_csv('Datasets/Location.csv')
-crime = pd.read_csv('Datasets/CrimeCount.csv')
-stations = pd.read_csv('Datasets/Stations.csv')
-trees = pd.read_csv('Datasets/Trees.csv')
-zip = pd.read_csv('Datasets/zip_neighbourhood.csv')
-bnb = pd.read_csv('Datasets/AirBnb.csv')
+ATTRACTION = pd.read_csv('Datasets/Location.csv')
+AIRBNB = pd.read_csv('Datasets/AirBnb.csv')
 
 
 def convert_price(price_str):
+    '''
+    convert the price in Airbnb dataframe from a string to an integers
+
+    Arg:
+        price (str) : the price as a string
+    
+    Returns:
+        int : the price converted to int
+    '''
     prezzo_float = float(price_str.replace('$', '').replace(',', ''))
 
     return int(prezzo_float)
 
-bnb['price'] = bnb['price'].apply(convert_price)
+AIRBNB['price'] = AIRBNB['price'].apply(convert_price)
 
 
 @app.get('/index/{key}')
 def get_air_quality(key):
+    '''
+    endpoint to get the most recent airquality data.
+
+    Args:
+        key (str) : the API key for OpenWether service. 
+    Returns: 
+        list : a list of 5 dictionary with the AQI value for each neighbourhood of NYC.
+    '''
     try:
         data = air_quality.air_quality(key)
         return data
@@ -37,9 +50,20 @@ def get_air_quality(key):
 
 @app.get('/search')
 def search_bnb(min, max, trees_bool, crime_rate ):
+    '''
+    endpoint to query the airbnb based on number of attractions that the users want to visit, if the users want to stay close to green areas, and how much the users care about crimes.
+    
+    Args: 
+        min (int) : the minimum amount of attraction that the user want to see.
+        max (int) : the maximum amount of attractin that the user want to see.
+        trees_bool (bool) : value that reflect if the user wants to stay in a green area or not.
+        crime_rate (int) : a value from 1 to 4 given by the user depending on how much he cares about crimes.
+
+    returns:
+        list : all the airbnb and their data that match all the criteria given by the user
+    '''
     
     #check if integer is between the correct range
-    
     try:
         min = int(min)
         max = int(max)
@@ -51,7 +75,7 @@ def search_bnb(min, max, trees_bool, crime_rate ):
             zipcodes_trees = data_handling.corrZipTrees(trees_bool)
             zipcodes_crime = data_handling.corrZipCrime(crime_rate)
             res = data_handling.commonZip(zipcodes_attr, zipcodes_crime, zipcodes_trees)
-            val = data_handling.BnbPerZip(res, bnb)
+            val = data_handling.BnbPerZip(res, AIRBNB)
             list_of_dicts = val.to_json(orient='records')
 
         return list_of_dicts
@@ -60,6 +84,15 @@ def search_bnb(min, max, trees_bool, crime_rate ):
 
 @app.get('/neighbourhood')
 def get_borough(neighbourhood):
+    '''
+    Endpoint to query aibnbs based on the neighbourhood.
+
+    Args: 
+        neighbourhood (str) : the name of a neighbourhood of NYC.
+    
+    Returns:
+        list : all the airbnb that are inside the given neighbourhood.
+    '''
 
     try:
         data = data_handling.get_bnb_by_neighborhood(neighbourhood)
@@ -71,6 +104,16 @@ def get_borough(neighbourhood):
 
 @app.get('/advanced')
 def airbnb_in_range(attractions, range):
+    '''
+    Endpoint to query the airbnbs inside a search area computed from the lists of attractions that the user want to visit.
+
+    Args: 
+        attractions (list) : the list of attractions selected by the user.
+        range (int) : the radius of the search area in meters.
+    
+    Returns:
+        list : all the airbnbs and their data that falls inside the search area.
+    '''
     
     try:
         attractions = literal_eval(attractions)
@@ -91,15 +134,29 @@ def airbnb_in_range(attractions, range):
 
 @app.get('/map')
 def get_map_data(attraction):
+    '''
+    Endpoint to get the latitude and longitude of all the selected attractions.
+
+    Arg:
+        attraction (list) : list of the attractions selected by the user.
+    
+    Returns:
+        list : all the selected attractions and their coordinates data.
+    '''
     attractions_list = literal_eval(attraction)
-    print(attractions_list)
-    extracted_attractions = attractions[attractions['Tourist_Spot'].isin(attractions_list)][['Tourist_Spot', 'Latitude', 'Longitude']]
+    extracted_attractions = ATTRACTION[ATTRACTION['Tourist_Spot'].isin(attractions_list)][['Tourist_Spot', 'Latitude', 'Longitude']]
     list_of_dicts = extracted_attractions.to_json(orient = 'records')
     return list_of_dicts
 
 
 @app.get('/attractions')
 def attraction_list():
+    '''
+    Endpoint to get the names of all the attractions from the attraction dataframe.
+
+    Returns: 
+        list : attraction id and attraction name for each attraction in the Location.csv file so we can use it in a form.
+    '''
     attractions = advanced_research.get_attractions_list()
 
     return attractions
